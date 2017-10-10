1b4abe6095952ca2e57e01719ee033bf64b28e88
Nazip <nazipm@mail.ru>
2017-10-10 13:24:30 +0300

    enzyme do  not work

--- stories/elements/CheckBox.js
+++ stories/elements/CheckBox.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { CheckBox } from 'components/elements';
 import { text, boolean } from '@storybook/addon-knobs';
+import { specs, describe, it } from 'storybook-addon-specifications';
+import { mount } from 'enzyme';
+// import expect from 'expect';
+
 
 export const CheckBoxStory = chapter => chapter
   .addWithInfo('CheckBox','CheckBox',
-    () => <CheckBox
-      name={'CheckBox name'}
-      checked={boolean('checked', false)}
-      onChange={action('Clicked')}
-      label={text('label','CheckBox label')}/>
+    () => {
+      const story = (<CheckBox
+        name={'CheckBox name'}
+        checked={boolean('checked', false)}
+        onChange={action('Clicked')}
+        label={text('label','CheckBox label')}/>);
+
+      // specs(() => describe('checkbox', () => {
+      //   it('should have label',() => {
+      //     const output = mount(story);
+      //     expect(output.txt()).toContain('CheckBox label');
+      //   });
+      // }));
+      return story;
+    }
+
   );
---
+++ utils/before_test.js
@@ -0,0 +1,3 @@
+var path = require('path');
+
+require('app-module-path').addPath(path.join(process.cwd(), 'src'));
